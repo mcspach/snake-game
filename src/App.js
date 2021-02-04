e55f17f583408739b8d7cfe91170dec1631b518f
Matthew Spach <YOSHISU@Matthews-MacBook-Pro.local>
2021-02-04 12:32:28 -0800

    add grid and bg

--- src/App.js
+++ src/App.js
@@ -1,25 +1,111 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>SNAKE ATTACK</p>
+        </header>
+        <div className="snake-container">
+          <div className="grid">
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
 }
 
 export default App;
