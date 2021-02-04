df4ec7f5d52171562442227292737bd99f736440
Matthew Spach <YOSHISU@Matthews-MacBook-Pro.local>
2021-02-04 15:50:22 -0800

    got rid of errors but need to debug the map

--- src/App.js
+++ src/App.js
@@ -2,7 +2,38 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      rows: 10,
+      cols: 10,
+      grid: [],
+    };
+  
+
+  const grid = [];
+  for ( let col = 0; col < this.state.cols; col++) {
+    for ( let row = 0; row < this.state.rows; row++) {
+      grid.push({
+        row,
+        col
+      })
+    }
+  }
+  this.setState({grid: grid});
+}
+
+
+
   render() {
+
+    const gridItems = this.state.grid.map((grid) => {
+      return ( 
+        <div key={grid.row.toString() + '-' + grid.col.toString()}
+        className="grid-item" ></div>
+      );
+    })
+
     return (
       <div className="App">
         <header className="App-header">
@@ -10,102 +41,12 @@ class App extends Component {
         </header>
         <div className="snake-container">
           <div className="grid">
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
+            {gridItems}
           </div>
         </div>
       </div>
     );
   }
-
 }
 
 export default App;
