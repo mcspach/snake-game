327c351bd86a3444b393528c0006c6ab2a120c64
Matthew Spach <YOSHISU@Matthews-MacBook-Pro.local>
2021-02-05 16:18:09 -0800

    grid working but looks like ass

--- src/App.js
+++ src/App.js
@@ -3,43 +3,91 @@ import './App.css';
 
 export default class App extends Component {
   state = {
-    height: 10,
-    width: 10,
-    grid: [],
+    columns: 20,
+    rows: 20,
+    scale: 10,
     score: 0,
+    direction: 'right',
+    lastDriection: null,
+    dead: false
   };
    
-  createGrid() {
-    const grid = [];
-    for ( let row = 0; row < this.state.rows; row++) {
-        for ( let col = 0; col < this.state.cols; col++) {
-        grid.push({
-          row: row,
-          col: col
-        });
-      this.setState({grid: grid});
-      }
-    }
-  }
+  componentDidMount() {
+    const container = document.getElementById('snake-container').parentElement;
+    const boardWidth = container.offsetWidth - 125;
+    const boardHeight = container.offsetHeight - 125;
 
+    const scale = Math.floor(boardWidth / this.state.columns)
+
+    this.setState({
+      boardWidth: boardWidth,
+      boardHeight: boardHeight,
+      scale: scale,
+      rows: Math.floor(boardHeight / scale),
+    }, () => this.restart());
+    console.log(this.state);
+    }
+
+    restart = () => {
+        this.setState({
+            direction: 'right',
+            grid: this.state.grid || this.getGrid(),
+            segments: [{
+                x: 2,
+                y: 0
+            }, {
+                x: 1,
+                y: 0
+            }, {
+                x: 0,
+                y: 0
+            }],
+            food: null,
+            lastDirection: null,
+            lastPosition  : {
+                x : 2,
+                y : 0
+            },
+            dead: false,
+            score: 0
+        });
+    };
+
+    getGrid = () => {
+      const borderColor = this.state.dead ? '#666' : '#ccc';
+      const tiles = [];
+      for (let y = 0; y < this.state.rows; y++) {
+        for (let x = 0; x < this.state.columns; x++) {
+            tiles.push((
+                <div
+                    key={"tile-" + x + '-' + y}
+                    style={{
+                        position : 'absolute',
+                        height   : this.state.scale,
+                        width    : this.state.scale,
+                        top      : this.state.scale * y,
+                        left     : this.state.scale * x,
+                        borderLeft : x > 0 ? `1px solid ${borderColor}` : 'none',
+                        borderBottom : y < this.state.columns - 1 ? `1px solid ${borderColor}` : 'none'
+                    }}
+                >
+                </div>
+            ));
+          }
+        }
+    return tiles;
+    }
 
   render() {
-
-    const gridItems = this.state.grid.map((grid) => {
-      return ( 
-        <div key={grid.row.toString() + '-' + grid.col.toString()}
-        className="grid-item" ></div>
-      );
-    })
 
     return (
       <div className="App">
         <header className="App-header">
           <p>SNAKE ATTACK</p>
         </header>
-        <div className="snake-container">
+        <div id="snake-container">
           <div className="grid">
-            {gridItems}
+            {this.state.grid}
           </div>
         </div>
       </div>
